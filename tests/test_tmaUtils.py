4791feb888a4f058ff22d8fd09acb50800d3d48a
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2023-07-19 11:00:59 -0700

    Update the instructions for updating the vcr data

--- tests/test_tmaUtils.py
+++ tests/test_tmaUtils.py
@@ -55,10 +55,13 @@ __all__ = [
     'writeNewTmaEventTestTruthValues',
 ]
 
-# Use mode="none" to run tests for normal operation.
-# To update files or generate new ones, make sure you have a working
-# connection to lsst-schema-registry-efd.ncsa.illinois.edu
-# and temporarily run with mode="once".
+# Use record_mode="none" to run tests for normal operation. To update files or
+# generate new ones, make sure you have a working connection to the EFD at all
+# the relevant sites, and temporarily run with mode="all" via *both*
+# python/pytest *and* with scons, as these generate slightly different HTTP
+# requests for some reason. Also make sure to do all this at both the summit
+# and USDF. The TTS is explicitly skipped and does not need to follow this
+# procedure.
 packageDir = getPackageDir('summit_utils')
 safe_vcr = vcr.VCR(
     record_mode="none",
