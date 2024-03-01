abc50bad4f990f896306c51abdc97f0aa676af7f
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-03-01 11:26:20 -0800

    Update saturation value

--- config/quickLookIsr.py
+++ config/quickLookIsr.py
@@ -3,3 +3,4 @@ config.doSaturation = True  # saturation very important for roundness measuremen
 config.doSaturationInterpolation = True
 config.overscan.fitType = 'MEDIAN_PER_ROW'
 config.overscan.doParallelOverscan = True
+config.saturation = 120000
