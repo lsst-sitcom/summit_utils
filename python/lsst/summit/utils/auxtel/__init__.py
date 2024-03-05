82f048072c16595fa424a3c89c0a532684b473de
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-03-05 14:05:49 -0800

    Stop importing * in __initi__.py

--- python/lsst/summit/utils/auxtel/__init__.py
+++ python/lsst/summit/utils/auxtel/__init__.py
@@ -19,4 +19,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .mount import *
+from .mount import hasTimebaseErrors
