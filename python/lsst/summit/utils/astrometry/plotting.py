e6c12969dcc71631e8a9049bbd4f78d093d5c489
Cristobal Pio Garcia <cpio@sdfrome002.sdf.slac.stanford.edu>
2024-03-11 08:35:44 -0700

    add method and function typing

--- python/lsst/summit/utils/astrometry/plotting.py
+++ python/lsst/summit/utils/astrometry/plotting.py
@@ -23,6 +23,7 @@ import copy
 import logging
 from typing import TYPE_CHECKING
 import logging
+from typing import TYPE_CHECKING
 
 import astropy.units as u
 import matplotlib.pyplot as plt
@@ -50,7 +51,11 @@ def plot(
     clipMin: float = 1,
     clipMax: float = 1000000,
     doSmooth: bool = True,
+<<<<<<< HEAD
     fig: matplotlib.Figure.figure | None = None,
+=======
+    fig: matplotlib.figure.Figure | None = None,
+>>>>>>> 775bbea (add method and function typing)
 ) -> None:
     """Plot an exposure, overlaying the selected sources and compass arrows.
 
