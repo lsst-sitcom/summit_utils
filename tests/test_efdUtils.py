9019b5e576b58868a9000cf90b217b345212da09
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-10-01 05:54:37 -0700

    Add protection against None client in tests

--- tests/test_efdUtils.py
+++ tests/test_efdUtils.py
@@ -86,7 +86,8 @@ class EfdUtilsTestCase(lsst.utils.tests.TestCase):
     @vcr.use_cassette()
     def tearDown(self):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.client.influx_client.close())
+        if self.client.influx_client is not None:
+            loop.run_until_complete(self.client.influx_client.close())
 
     @vcr.use_cassette()
     def test_makeEfdClient(self):
