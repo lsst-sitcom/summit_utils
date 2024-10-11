3c23b414c07413a077a70b49a02538fc3c14234a
elanaku <eurbach@g.harvard.edu>
2024-10-11 16:41:25 -0300

    Add listener steps

--- python/lsst/summit/utils/kafka_attempt.py
+++ python/lsst/summit/utils/kafka_attempt.py
@@ -1,4 +1,6 @@
 import requests
+from kafka import KafkaConsumer
+import json
 
 
 def create_kafka_postISRmedian_topics():
@@ -133,3 +135,146 @@ def post_to_sasquatch_comcam_isr(
 
     response = requests.request("POST", url, json=payload, headers=headers)
     print(response.text)
+
+
+""" Making the listener"""
+
+
+def listen_to_kafka(topic, key, group_id, broker):
+    """
+    topic is Topic name, e.g. 'lsst.dm.latiss.postIsrPixelMedian'
+    key is key e.g. postIsrPixelMedian
+    """
+
+    # Kafka consumer configuration
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=broker,  # Kafka broker(s) - e.g. ['localhost:9092']
+        group_id=group_id,  # Consumer group ID (for load balancing) - no clue
+        auto_offset_reset="latest",  # Start reading from the latest message (ignore past messages)
+        enable_auto_commit=True,  # Automatically commit the offsets
+        value_deserializer=lambda m: json.loads(m.decode("utf-8")),  # Deserialize JSON messages
+    )
+
+    print("Waiting for Kafka messages...")
+
+    # Infinite loop to keep the consumer active
+    for message in consumer:
+        # This block is executed as soon as a message is received
+        print(f"New message received: {message.value}")
+
+        # Extract a value from the message
+        value = message.value.get(key)
+        print(f"Extracted value: {value}")
+        return value
+
+        # Perform your desired processing on the message
+
+
+"""
+# Kafka user - not sure where this lives
+
+apiVersion: kafka.strimzi.io/v1beta2
+kind: KafkaUser
+metadata:
+  name: toyapp
+  labels:
+    # The name of the Strimzi ``Kafka`` resource, probably "sasquatch"
+    strimzi.io/cluster: sasquatch
+
+  authentication:
+    # This should always be "tls"
+    type: tls
+
+  authorization:
+    type: simple
+    acls:
+
+      # If your app consumes messages, this gives permission to consume as
+      # part of any consumer group that starts with the named prefix.
+      - resource:
+          type: group
+          name: "lsst.dm"
+          patternType: prefix
+        operations:
+          - "Read"
+        host: "*"
+
+# Kafka access - not sure where this lives either
+
+apiVersion: access.strimzi.io/v1alpha1
+kind: KafkaAccess
+metadata:
+  name: toyapp-kafka
+spec:
+  kafka:
+    # The name and namespace of the Strimzi ``Kafka`` resource, probably
+    # "sasquatch", but not sure here...
+    name: sasquatch
+    namespace: sasquatch
+    # This should always be "tls"
+    listener: tls
+  user:
+    kind: KafkaUser
+    apiGroup: kafka.strimzi.io
+    # This is the name of the ``KafkaUser`` that you created
+    name: toyapp
+    # This is the namespace of the ``KafkaUser``, NOT your app's namespace,
+    # probably "sasquatch"
+    namespace: sasquatch
+
+
+# providing credentials... this lives in the ap's container...
+
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+ ...
+  name: toyapp
+  namespace: toyapp
+spec:
+  ...
+  template:
+    ...
+    spec:
+      containers:
+      - env:
+        - name: KAFKA_SECURITY_PROTOCOL
+            secretKeyRef:
+              key: securityProtocol
+              name: myapp-kafka
+        - name: KAFKA_BOOTSTRAP_SERVERS
+          valueFrom:
+            secretKeyRef:
+              key: bootstrapServers
+              name: myapp-kafka
+        - name: KAFKA_CLUSTER_CA_PATH
+          value: /etc/kafkacluster/ca.crt
+        - name: KAFKA_CLIENT_CERT_PATH
+          value: /etc/kafkauser/user.crt
+        - name: KAFKA_CLIENT_KEY_PATH
+          value: /etc/kafkauser/user.key
+
+        ...
+
+        volumeMounts:
+        - mountPath: /etc/kafkacluster/ca.crt
+          name: kafka
+          subPath: ssl.truststore.crt
+        - mountPath: /etc/kafkauser/user.crt
+          name: kafka
+          subPath: ssl.keystore.crt
+        - mountPath: /etc/kafkauser/user.key
+          name: kafka
+          subPath: ssl.keystore.key
+
+      ...
+
+      volumes:
+      - name: kafka
+        secret:
+          defaultMode: 420
+          # The ``metadata.name`` value from the ``KafkaAccess`` resource in
+          # your app's namespace
+          secretName: toyapp-kafka
+"""
