8de8efb9b439593211743357ec0dc78334c4ed7e
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-11-25 10:48:59 -0800

    Add convenience function for getting the sun angle

--- python/lsst/summit/utils/utils.py
+++ python/lsst/summit/utils/utils.py
@@ -29,8 +29,7 @@ import matplotlib
 import numpy as np
 import numpy.typing as npt
 from astro_metadata_translator import ObservationInfo
-from astropy.coordinates import AltAz, SkyCoord
-from astropy.coordinates.earth import EarthLocation
+from astropy.coordinates import AltAz, EarthLocation, SkyCoord, get_sun
 from astropy.time import Time
 from dateutil.tz import gettz
 from deprecated.sphinx import deprecated
@@ -52,7 +51,7 @@ from lsst.afw.detection import Footprint, FootprintSet
 from lsst.daf.butler.cli.cliLog import CliLog
 from lsst.obs.lsst import Latiss, LsstCam, LsstComCam, LsstComCamSim
 from lsst.obs.lsst.translators.latiss import AUXTEL_LOCATION
-from lsst.obs.lsst.translators.lsst import FILTER_DELIMITER
+from lsst.obs.lsst.translators.lsst import FILTER_DELIMITER, SIMONYI_LOCATION
 
 from .astrometry.utils import genericCameraHeaderToWcs
 
@@ -96,6 +95,7 @@ __all__ = [
     "computeExposureId",
     "computeCcdExposureId",
     "getDetectorIds",
+    "getSunAngle",
 ]
 
 
@@ -1348,3 +1348,17 @@ def calcEclipticCoords(ra: float, dec: float) -> tuple[float, float]:
         lambda_ += np.pi * 2
 
     return (np.rad2deg(lambda_), np.rad2deg(beta))
+
+
+def getSunAngle() -> float:
+    """Get the angle of the sun to the horizon. Positive numbers means the
+    sun is above the horizon, negative means it is below.
+
+    Returns
+    -------
+    sun_alt : `float`
+        The angle of the sun to the horizon, in degrees.
+    """
+    now = Time.now()
+    sun_altaz = get_sun(now).transform_to(AltAz(obstime=now, location=SIMONYI_LOCATION))
+    return sun_altaz.alt.deg
