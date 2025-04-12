22f556c8d832f3e232f069589309d06902671ab8
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2025-04-12 09:08:04 -0700

    Add size of data range to quantile warning message

--- python/lsst/summit/utils/utils.py
+++ python/lsst/summit/utils/utils.py
@@ -1131,10 +1131,11 @@ def getQuantiles(data: npt.NDArray[np.float64], nColors: int) -> npt.NDArray[np.
         A monotonically increasing sequence of size (nColors + 1). These are
         the edges of nColors intervals.
     """
-    if (np.nanmax(data) - np.nanmin(data)) > 300_000:
+    dataRange = np.nanmax(data) - np.nanmin(data)
+    if dataRange > 300_000:
         # Use slower but memory efficient nanquantile
         logger = logging.getLogger(__name__)
-        logger.warning("Data range is very large; using slower quantile code.")
+        logger.warning(f"Data range is very large ({dataRange}); using slower quantile code.")
         boundaries = np.nanquantile(data, np.linspace(0, 1, nColors + 1))
     else:
         cdf, minVal, maxVal = getCdf(data, nColors)
