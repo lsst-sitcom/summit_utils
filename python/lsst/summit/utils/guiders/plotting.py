ace99ba3fb094d2f714125e427784c32feb72e6f
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2025-05-25 20:06:03 -0700

    Run a linter and fix some basics things

--- python/lsst/summit/utils/guiders/plotting.py
+++ python/lsst/summit/utils/guiders/plotting.py
@@ -20,16 +20,15 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-from astropy.stats import mad_std
 import numpy as np
 
-__all__ = ["plotGuiderCCDStamps"]
+__all__ = ["PlotGuiderCCDStamps"]
 
-class plotGuiderCCDStamps:
+
+class PlotGuiderCCDStamps:
     """Class to read and unpack the Guider data from Butler.
        Plot an animated gif of the CCD guider stamp.
-    
+
     Example:
         from lsst.summit.utils.guiders.reading import readGuiderData
         from lsst.summit.utils.guiders.plotting import plotGuiderCCDStamps
@@ -41,20 +40,21 @@ class plotGuiderCCDStamps:
         reader = readGuiderData(seqNum, dayObs, view='dvcs', verbose=True)
         reader.init_guiders()
         reader.load_data()
-        
+
         # Create the plotter object
         plotter = plotGuiderCCDStamps(reader)
-        
+
         # Plot a stacked image of the stamps
         plotter.plot_stacked_stamp_array()
-        
+
         # Plot a single stamp
         plotter.plot_stamp_array(stampNum=9)
-        
+
         # Make a gif of the stamps
         plotter.make_gif(nStampsMax=50, fps=10)
     """
-    def __init__(self, reader, butler=None, view='dvcs'):
+
+    def __init__(self, reader, butler=None, view="dvcs"):
         # reader object readGuiderData
         self.reader = reader
         self.view = reader.view
@@ -65,28 +65,28 @@ class plotGuiderCCDStamps:
         self.detnames = reader.getGuiderNames()
 
         # for plotting
-        self.layout =  [
-                        [      ".", "R40_SG1", "R44_SG0",       "."],
-                        ["R40_SG0",       "center",       ".", "R44_SG1"],
-                        ["R00_SG1",       ".",       ".", "R04_SG0"],
-                        [      ".", "R00_SG0", "R04_SG1",       "."],
-                    ]
+        self.layout = [
+            [".", "R40_SG1", "R44_SG0", "."],
+            ["R40_SG0", "center", ".", "R44_SG1"],
+            ["R00_SG1", ".", ".", "R04_SG0"],
+            [".", "R00_SG0", "R04_SG1", "."],
+        ]
 
-    def plot_stamp_ccd(self, raft_ccd_key, stampNum=-1, axs=None, plo = 10.0, phi = 99.0):
+    def plot_stamp_ccd(self, raft_ccd_key, stampNum=-1, axs=None, plo=10.0, phi=99.0):
         if axs is None:
             axs = plt.gca()
             plt.title(f"{self.expId}")
-        
+
         if stampNum < 0:
             img = self.reader.read_stacked(raft_ccd_key)
         else:
             img = self.reader.read(stampNum, raft_ccd_key)
-        
+
         bias = np.median(img)
         img_isr = img - bias
-        lo,hi = np.nanpercentile(img_isr,[plo,phi])
-    
-        im = axs.imshow(img_isr,origin='lower',cmap='Greys',vmin=lo,vmax=hi, animated=True)
+        lo, hi = np.nanpercentile(img_isr, [plo, phi])
+
+        im = axs.imshow(img_isr, origin="lower", cmap="Greys", vmin=lo, vmax=hi, animated=True)
         axs.set_yticklabels([])
         axs.set_xticklabels([])
         axs.set_xticks([])
@@ -96,10 +96,10 @@ class plotGuiderCCDStamps:
         return im
 
     def get_stamp_number_info(self, stampNum=0):
-        text =  f"day_obs: {self.dayObs}"+ "\n"+f"seq_num: {self.seqNum}"+"\n"
-        text += f"orientation: {self.view}"+"\n"
-        if stampNum>0:
-            text += f"Stamp #: {stampNum+1:02d}"
+        text = f"day_obs: {self.dayObs}" + "\n" + f"seq_num: {self.seqNum}" + "\n"
+        text += f"orientation: {self.view}" + "\n"
+        if stampNum > 0:
+            text += f"Stamp #: {stampNum + 1:02d}"
         else:
             text += f"Stacked Image w/ {self.nStamps} stamps"
         return text
@@ -107,20 +107,25 @@ class plotGuiderCCDStamps:
     def plot_stamp_info(self, stampNum=0, axs=None, more_text=None):
         if axs is None:
             axs = plt.gca()
-            
-        axs.set_xticks([]); axs.set_yticks([])
+
+        axs.set_xticks([])
+        axs.set_yticks([])
         axs.set_axis_off()
 
         text = self.get_stamp_number_info(stampNum)
         if more_text is not None:
-            text += "\n"+more_text
-            
+            text += "\n" + more_text
+
         stamp_id_text = axs.text(
-            1.085, -0.10, text,
+            1.085,
+            -0.10,
+            text,
             transform=axs.transAxes,
-            ha='center', va='center',
-            fontsize=14, color='firebrick',
-            animated=True
+            ha="center",
+            va="center",
+            fontsize=14,
+            color="firebrick",
+            animated=True,
         )
         axs.set_axis_off()
         self.stamp_id_axs = stamp_id_text
@@ -130,16 +135,20 @@ class plotGuiderCCDStamps:
     def plot_text_ccd_name(self, detname, axs=None):
         if axs is None:
             axs = plt.gca()
-        txt = axs.text(0.025, 0.025, detname,
-                       transform=axs.transAxes,
-                       ha='left', va='bottom',
-                       fontsize=9,
-                       weight='bold',
-                       color='grey'
-                       )
+        txt = axs.text(
+            0.025,
+            0.025,
+            detname,
+            transform=axs.transAxes,
+            ha="left",
+            va="bottom",
+            fontsize=9,
+            weight="bold",
+            color="grey",
+        )
         return txt
 
-    def plot_stamp_array(self, stampNum=0, fig=None, axs=None, plo=90., phi=99.):
+    def plot_stamp_array(self, stampNum=0, fig=None, axs=None, plo=90.0, phi=99.0):
         """Plot the stamp array for all the guiders.
         Args:
             stampNum (int): stamp number
@@ -148,17 +157,19 @@ class plotGuiderCCDStamps:
         """
         if fig is None:
             gs = dict(hspace=0.0, wspace=0.0)
-            fig, axs = plt.subplot_mosaic(self.layout, figsize=(9.5, 9.5), gridspec_kw=gs, constrained_layout=False)
+            fig, axs = plt.subplot_mosaic(
+                self.layout, figsize=(9.5, 9.5), gridspec_kw=gs, constrained_layout=False
+            )
 
         artists = []
         for detname in self.detnames:
-            im = self.plot_stamp_ccd(detname, stampNum=stampNum, axs=axs[detname], plo=plo,phi=phi)
+            im = self.plot_stamp_ccd(detname, stampNum=stampNum, axs=axs[detname], plo=plo, phi=phi)
             txt = self.plot_text_ccd_name(detname, axs=axs[detname])
             artists.extend([im, txt])
-        stamp_info = self.plot_stamp_info(axs=axs['center'], stampNum=stampNum)
+        stamp_info = self.plot_stamp_info(axs=axs["center"], stampNum=stampNum)
         artists.append(stamp_info)
         return artists
-    
+
     def plot_stacked_stamp_array(self, fig=None, axs=None):
         """Plot the stamp array for all the guiders.
         Args:
@@ -171,59 +182,63 @@ class plotGuiderCCDStamps:
 
     def make_gif(self, nStampsMax=10, fps=5, dpi=80):
         from matplotlib import animation
-        # Create the animation
-        fig, axs = plt.subplot_mosaic(self.layout, figsize=(9.5, 9.5), gridspec_kw=dict(hspace=0.0, wspace=0.0), constrained_layout=False)
 
-        nStamps = min( self.nStamps, nStampsMax )
+        # Create the animation
+        fig, axs = plt.subplot_mosaic(
+            self.layout,
+            figsize=(9.5, 9.5),
+            gridspec_kw=dict(hspace=0.0, wspace=0.0),
+            constrained_layout=False,
+        )
+
+        nStamps = min(self.nStamps, nStampsMax)
         print("Number of stamps: ", nStamps)
 
         # plot the stacked image
         artists0 = self.plot_stacked_stamp_array(fig=fig, axs=axs)
-        frame_list = 5*[artists0]
+        frame_list = 5 * [artists0]
 
-        #loop over the stamps
+        # loop over the stamps
         for i in range(nStamps):
             artists = self.plot_stamp_array(stampNum=i, fig=fig, axs=axs)
             frame_list.append(artists)
-        
-        frame_list+= 5*[artists0]
+
+        frame_list += 5 * [artists0]
 
         # update the stamps
-        ani = animation.ArtistAnimation(
-            fig, frame_list,
-            interval=1000/fps, blit=True,
-            repeat_delay=1000
-        )
+        ani = animation.ArtistAnimation(fig, frame_list, interval=1000 / fps, blit=True, repeat_delay=1000)
         ani.save(f"guider_ccd_array_{self.expId}.gif", fps=fps, dpi=dpi, writer="pillow")
         plt.close(fig)
         return ani
 
     def make_mp4(self, nStampsMax=10, fps=5, dpi=80):
         from matplotlib import animation
-        # Create the animation
-        fig, axs = plt.subplot_mosaic(self.layout, figsize=(9.5, 9.5), gridspec_kw=dict(hspace=0.0, wspace=0.0), constrained_layout=False)
 
-        nStamps = min( self.nStamps, nStampsMax )
+        # Create the animation
+        fig, axs = plt.subplot_mosaic(
+            self.layout,
+            figsize=(9.5, 9.5),
+            gridspec_kw=dict(hspace=0.0, wspace=0.0),
+            constrained_layout=False,
+        )
+
+        nStamps = min(self.nStamps, nStampsMax)
         print("Number of stamps: ", nStamps)
 
         # plot the stacked image
         artists0 = self.plot_stacked_stamp_array(fig=fig, axs=axs)
-        frame_list = 5*[artists0]
+        frame_list = 5 * [artists0]
 
-        #loop over the stamps
+        # loop over the stamps
         for i in range(nStamps):
             artists = self.plot_stamp_array(stampNum=i, fig=fig, axs=axs)
             frame_list.append(artists)
-        frame_list+= 5*[artists0]
+        frame_list += 5 * [artists0]
 
         # update the stamps
-        ani = animation.ArtistAnimation(
-            fig, frame_list,
-            interval=1000/fps, blit=True,
-            repeat_delay=1000
-        )
+        ani = animation.ArtistAnimation(fig, frame_list, interval=1000 / fps, blit=True, repeat_delay=1000)
         ani.save(f"guider_ccd_array_{self.expId}.mp4", fps=fps, dpi=dpi)
         plt.close(fig)
         return ani
-    
+
     # TODO: add the Alt/Az orientation
--- python/lsst/summit/utils/guiders/reading.py
+++ python/lsst/summit/utils/guiders/reading.py
@@ -21,74 +21,70 @@
 
 __all__ = [
     "read_guider_data",
-    "readGuiderData",
+    "ReadGuiderData",
     "get_guider_stamps",
 ]
 
-import numpy as np
-import matplotlib.pyplot as plt
-from astropy.time import Time
 import astropy.units as u
-
+import numpy as np
 from astropy.io import fits
-from lsst.resources import ResourcePath
+from astropy.time import Time
 
-from lsst.obs.lsst import LsstCam
-from lsst.daf.butler import Butler
 from lsst.afw import cameraGeom
+from lsst.afw.image import MaskedImageF
+from lsst.daf.butler import Butler
+from lsst.meas.algorithms.stamps import Stamp, Stamps
+from lsst.obs.lsst import LsstCam
+from lsst.resources import ResourcePath
+from lsst.summit.utils.guiders.transformation import convert_roi, mk_roi_bboxes
 from lsst.summit.utils.utils import getSite
+
 # Get the site and the camera object
 site = getSite()
 camera = LsstCam.getCamera()
 
-from lsst.meas.algorithms.stamps import Stamp,Stamps
-from lsst.afw.image import MaskedImageF
-
-from lsst.summit.utils.guiders.transformation import mk_roi_bboxes, convert_roi
-
 # Todo: put in summit utils guiders
-#from utils import get_guider_stamps
+# from utils import get_guider_stamps
 
-from astropy.io import fits
-from lsst.resources import ResourcePath
 
 def read_guider_data(filename):
     rp = ResourcePath(filename)
-    
+
     with rp.open(mode="rb") as f:
-        
+
         hdu_list = fits.open(f)
-        
-        N_frames = hdu_list[0].header['N_STAMPS']
-        x, y = hdu_list[0].header['ROIROWS'], hdu_list[0].header['ROICOLS']
+
+        N_frames = hdu_list[0].header["N_STAMPS"]
+        x, y = hdu_list[0].header["ROIROWS"], hdu_list[0].header["ROICOLS"]
 
         imgs = np.zeros((N_frames, x, y))
         timestamps = np.zeros(N_frames)
 
-        if (len(hdu_list) - 1)/2 == N_frames:  ## if has rawstamps
+        if (len(hdu_list) - 1) / 2 == N_frames:  # if has rawstamps
             for i, j in zip(range(N_frames), range(2, len(hdu_list), 2)):
                 imgs[i, :, :] = hdu_list[j].data
-                timestamps[i] = hdu_list[j].header['STMPTMJD']
+                timestamps[i] = hdu_list[j].header["STMPTMJD"]
 
-        elif len(hdu_list)-1 == N_frames:     ## if no rawstamps
+        elif len(hdu_list) - 1 == N_frames:  # if no rawstamps
             for i, j in zip(range(N_frames), range(1, len(hdu_list))):
                 imgs[i, :, :] = hdu_list[j].data
-                timestamps[i] = hdu_list[j].header['STMPTMJD']
-    
+                timestamps[i] = hdu_list[j].header["STMPTMJD"]
+
     grand_hdr = hdu_list[0].header
 
     return imgs, timestamps, grand_hdr
-    
-class readGuiderData:
+
+
+class ReadGuiderData:
     """Class to read and unpack the Guider data from Butler.
        Plot an animated gif of the CCD guider stamp.
-    
+
     Works in the summit and usdf environments.
-    
+
     Key Attributes:
         dataset (dict): Dictionary of guider data
         guiders (dict): Dictionary of guider detector information
-    
+
     Example:
         from lsst.summit.utils.guiders.reading import readGuiderData
 
@@ -100,7 +96,7 @@ class readGuiderData:
         reader.load_data()
 
         # reader makes a dictionary of the guider data
-        # with the keys being the guider names 
+        # with the keys being the guider names
         # and the values being the stamps object
         # the stamps orientation are defined by view, default 'dvcs'
         print("Guider names: ", reader.getGuiderNames())
@@ -110,31 +106,39 @@ class readGuiderData:
 
         # Create the plotter object
         plotter = plotGuiderCCDStamps(reader)
-        
+
         # Plot the initial stamp
         plotter.plot_stamp_array(stampNum=0, nStampsMax=10)
 
         # Make a gif of the stamps
         plotter.make_gif(nStampsMax=10, fps=5)
-        
+
     """
-    def __init__(self, seqNum, dayObs, verbose=False,
-                 butler=None, view='dvcs', collections=['LSSTCam/raw/all', 'LSSTCam/raw/guider']):
-        
+
+    def __init__(
+        self,
+        seqNum,
+        dayObs,
+        verbose=False,
+        butler=None,
+        view="dvcs",
+        collections=["LSSTCam/raw/all", "LSSTCam/raw/guider"],
+    ):
+
         # data id
         self.butler = butler
         self.dayObs = dayObs
         self.seqNum = seqNum
-        self.dataId = {'instrument': 'LSSTCam', 'day_obs': self.dayObs, 'seq_num': self.seqNum}
+        self.dataId = {"instrument": "LSSTCam", "day_obs": self.dayObs, "seq_num": self.seqNum}
 
         # exposure
-        self.expId = dayObs * 100000 + seqNum        
+        self.expId = dayObs * 100000 + seqNum
 
         # Define camera objects
         self.camera = LsstCam.getCamera()
 
-        self.FREQ = 5 # Hz
-        self.DELAY = 20/1000 # seconds
+        self.FREQ = 5  # Hz
+        self.DELAY = 20 / 1000  # seconds
 
         self.view = view
         self.dataset = None
@@ -147,7 +151,7 @@ class readGuiderData:
     def load(self):
         # Initialize the attributes
         self.init_guiders()
-        
+
         # Load the data
         self.load_data()
         self.get_header_info(self.dataset[self.detnames[0]])
@@ -158,24 +162,23 @@ class readGuiderData:
             self.print_header_info()
 
     def initialize_butler(self, collections=None):
-        if site=="summit":  
+        if site == "summit":
             repo = "LSSTCam"
-        elif site=="staff-rsp":
+        elif site == "staff-rsp":
             repo = "/repo/embargo"
         else:
             raise ValueError(f"Unknown butler repo for {site}")
-        
+
         if self.butler is None:
             # Initialize butler
-            self.butler = Butler(repo, collections=["LSSTCam/raw/guider"]+collections, instrument="LSSTCam")
+            self.butler = Butler(repo, collections=["LSSTCam/raw/guider"] + collections, instrument="LSSTCam")
         pass
-    
+
     def init_guiders(self):
-        """Load the guider detector information.
-        """
+        """Load the guider detector information."""
         self.guiders = {}
         for detector in self.camera:
-            if detector.getType()== cameraGeom.DetectorType.GUIDER :
+            if detector.getType() == cameraGeom.DetectorType.GUIDER:
                 detName = detector.getName()
                 self.guiders[detName] = detector.getId()
 
@@ -183,7 +186,6 @@ class readGuiderData:
         self.nGuiders = len(self.guiders)
         pass
 
-    
     def load_data(self):
         """Load the data from the butler for all guider detectors.
 
@@ -196,20 +198,23 @@ class readGuiderData:
         datas = {}
         dataId = self.dataId.copy()
         for detName, idet in self.guiders.items():
-            det = self.camera[idet]
-            if self.view == 'roi':
-                dataId['detector'] = idet
-                datas[detName] = self.butler.get('guider_raw', dataId)
-            
-            elif self.view == 'dvcs':
-                datas[detName] = get_guider_stamps(idet,self.seqNum,self.dayObs,butler=self.butler,view='dvcs')
-            
-            elif self.view == 'ccd':
-                datas[detName] = get_guider_stamps(idet,self.seqNum,self.dayObs,butler=self.butler,view='ccd')
+            if self.view == "roi":
+                dataId["detector"] = idet
+                datas[detName] = self.butler.get("guider_raw", dataId)
+
+            elif self.view == "dvcs":
+                datas[detName] = get_guider_stamps(
+                    idet, self.seqNum, self.dayObs, butler=self.butler, view="dvcs"
+                )
+
+            elif self.view == "ccd":
+                datas[detName] = get_guider_stamps(
+                    idet, self.seqNum, self.dayObs, butler=self.butler, view="ccd"
+                )
 
         self.dataset = datas
         self.init_ampnames()
-        #self.nStamps = max([len(data) for data in datas.values()])
+        # self.nStamps = max([len(data) for data in datas.values()])
         pass
 
     def init_ampnames(self):
@@ -217,8 +222,8 @@ class readGuiderData:
         for detname in self.guiders.keys():
             md = self.dataset[detname].metadata.toDict()
             # also get the ampName
-            segment = md['ROISEG']
-            ampName = 'C'+ segment[7:]        
+            segment = md["ROISEG"]
+            ampName = "C" + segment[7:]
             self.ampNames[detname] = ampName
         pass
 
@@ -227,29 +232,32 @@ class readGuiderData:
         self.stamp = np.arange(self.nStamps, dtype=float)
 
         # 2) parse start time once
-        t0 = Time(self.start_time, format='isot', scale='utc')
+        t0 = Time(self.start_time, format="isot", scale="utc")
 
         # 3) build Time array
         dt = (1.0 / self.FREQ) * u.second
-        dt+= self.DELAY * u.second
+        dt += self.DELAY * u.second
         self.timestamp = t0 + self.stamp * dt
         return self.timestamp
-    
+
     def get_header_info(self, raw):
         m = raw.metadata.toDict()
-        self.roiCol    = m['ROICOL']
-        self.roiRow    = m['ROIROW']
-        self.roiCols   = m['ROICOLS']
-        self.roiRows   = m['ROIROWS']
-        self.roiUnder  = m.get('ROIUNDER', m.get('ROIUNDRC', 6))
-        self.nStamps   = m['N_STAMPS']
-        self.start_time= m['GDSSTART']
-        self.FREQ      = 5  # 5 Hz
+        self.roiCol = m["ROICOL"]
+        self.roiRow = m["ROIROW"]
+        self.roiCols = m["ROICOLS"]
+        self.roiRows = m["ROIROWS"]
+        self.roiUnder = m.get("ROIUNDER", m.get("ROIUNDRC", 6))
+        self.nStamps = m["N_STAMPS"]
+        self.start_time = m["GDSSTART"]
+        self.FREQ = 5  # 5 Hz
         pass
 
     def print_header_info(self):
         print(f"Data Id: {self.dataId}")
-        print(f"ROI Row: {self.roiRow}, ROI Col: {self.roiCol}, ROI Rows: {self.roiRows}, ROI Cols: {self.roiCols}")
+        print(
+            f"ROI Row: {self.roiRow}, ROI Col: {self.roiCol}, ROI Rows: {self.roiRows},"
+            f" ROI Cols: {self.roiCols}"
+        )
         print(f"Number of Stamps: {self.nStamps}")
         print(f"Acq. Start Time: {self.start_time}")
         pass
@@ -272,7 +280,7 @@ class readGuiderData:
 
         roiarr = stamps[stamp].stamp_im.image.array
         return roiarr
-    
+
     def read_stacked(self, detname):
         """
         Read the Guider data from Butler and return image array.
@@ -288,30 +296,28 @@ class readGuiderData:
         for stamp in stamps:
             roiarr.append(stamp.stamp_im.image.array)
         return np.median(roiarr, axis=0)
-    
+
     def getGuiderNames(self):
-        """Get the names of the guider detectors.
-        """
+        """Get the names of the guider detectors."""
         return list(self.guiders.keys())
-    
+
     def getGuiderIds(self):
-        """Get the ids of the guider detectors.
-        """
+        """Get the ids of the guider detectors."""
         return list(self.guiders.values())
-    
+
     def getGuiderAmpNames(self):
         """
         Get list of guider amp names
         """
-        if hasattr(self, 'ampNames'):
+        if hasattr(self, "ampNames"):
             return list(self.ampNames.values())
 
         ampNames = {}
         for detname in self.guiders.keys():
             md = self.dataset[detname].metadata.toDict()
             # also get the ampName
-            segment = md['ROISEG']
-            ampName = 'C'+ segment[7:]        
+            segment = md["ROISEG"]
+            ampName = "C" + segment[7:]
             ampNames[detname] = ampName
         self.ampNames = ampNames
         return list(ampNames.values())
@@ -322,24 +328,27 @@ class readGuiderData:
         """
         if detname not in self.guiders.keys():
             raise ValueError(f"Guider {detname} not found.")
-        
+
         self.detname = detname
         self.idet = self.guiders[detname]
         self.detector = self.camera[self.idet]
         self.ampName = self.ampNames[detname]
         pass
 
-def get_guider_stamps(idet,seqNum,dayObs,repo='/repo/embargo',collections=['LSSTCam/raw/guider'],butler=None,view='dvcs'):
+
+def get_guider_stamps(
+    idet, seqNum, dayObs, repo="/repo/embargo", collections=["LSSTCam/raw/guider"], butler=None, view="dvcs"
+):
     """
-    This class reads the stamp object from the Butler for one Guiders and 
+    This class reads the stamp object from the Butler for one Guiders and
     converts them to DVCS view, making a new Stamps object
-    
+
     Parameters
     ----------
     idet : int
         Detector Id
-        
-    seqNum : int 
+
+    seqNum : int
         Sequence Number
 
     dayObs : int
@@ -355,65 +364,64 @@ def get_guider_stamps(idet,seqNum,dayObs,repo='/repo/embargo',collections=['LSST
     -------
     stamps : lsst.meas.algorithms.stamps.Stamps
         Stamp images oriented in DVCS
-    
+
     """
     # get Camera object
     camera = LsstCam.getCamera()
     detector = camera[idet]
-    
+
     # Get a Butler if none is provided
-    if butler==None:
+    if butler is None:
         butler = Butler(repo, collections=collections)
 
-
-    # for dayObs of 20250509 or before, the ROIs are swapped between SG0 and SG1.  Fix here
+    # for dayObs of 20250509 or before, the ROIs are swapped between SG0 and
+    # SG1.  Fix here
     if dayObs < 20250509:
         detName = camera[idet].getName()
         raft = detName[0:3]
         ccd = detName[4:7]
-        if ccd=='SG0':
-            ccd_swapped = 'SG1'
-        elif ccd=='SG1':
-            ccd_swapped = 'SG0'
+        if ccd == "SG0":
+            ccd_swapped = "SG1"
+        elif ccd == "SG1":
+            ccd_swapped = "SG0"
 
-        detName_swapped = raft + '_' + ccd_swapped
+        detName_swapped = raft + "_" + ccd_swapped
         detector_swapped = camera[detName_swapped]
         idet_swapped = detector_swapped.getId()
 
-        dataId = {'instrument': 'LSSTCam', 'detector': idet_swapped, 'day_obs': dayObs,'seq_num':seqNum}
+        dataId = {"instrument": "LSSTCam", "detector": idet_swapped, "day_obs": dayObs, "seq_num": seqNum}
 
     else:
-        dataId = {'instrument': 'LSSTCam', 'detector': idet, 'day_obs': dayObs,'seq_num':seqNum}
-
+        dataId = {"instrument": "LSSTCam", "detector": idet, "day_obs": dayObs, "seq_num": seqNum}
 
     # finally read from the Butler
-    raw_stamps = butler.get('guider_raw', dataId)
+    raw_stamps = butler.get("guider_raw", dataId)
     md = raw_stamps.metadata
 
     # fix CCD in the metadata
     if dayObs < 20250509:
-        md['CCDSLOT'] = ccd_swapped        
+        md["CCDSLOT"] = ccd_swapped
 
     # also get the ampName
-    segment = md['ROISEG']
-    ampName = 'C'+ segment[7:]
+    segment = md["ROISEG"]
+    ampName = "C" + segment[7:]
 
     # build the CCD view and DVCS view Bounding Boxes
-    ccd_view_bbox,dvcs_view_bbox = mk_roi_bboxes(md,camera)
-    
-    # now loop over the individual ROIs 
-    stamp_list= []
-    for i,masked_ims in enumerate(raw_stamps.getMaskedImages()):        
+    ccd_view_bbox, dvcs_view_bbox = mk_roi_bboxes(md, camera)
+
+    # now loop over the individual ROIs
+    stamp_list = []
+    for i, masked_ims in enumerate(raw_stamps.getMaskedImages()):
 
         # convert to DVCS view
         raw_roi = masked_ims.getImage().getArray()
-        roi_dvcs = convert_roi(raw_roi,detector,ampName,camera,view=view)
-        
+        roi_dvcs = convert_roi(raw_roi, detector, ampName, camera, view=view)
+
         # build a Stamp Object
         output_masked_im = MaskedImageF(roi_dvcs)
-        archive_element = [ccd_view_bbox,dvcs_view_bbox]
-        stamp_list.append(Stamp(output_masked_im,archive_element))
-    
-    output_stamps = Stamps(stamp_list,md,use_archive=True)
+        archive_element = [ccd_view_bbox, dvcs_view_bbox]
+        stamp_list.append(Stamp(output_masked_im, archive_element))
+
+    output_stamps = Stamps(stamp_list, md, use_archive=True)
 
     return output_stamps
