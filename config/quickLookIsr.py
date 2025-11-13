9a1a8c35c5d122e7c7673f9615ff0fefee2de0c7
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2025-11-13 14:15:41 -0800

    Remove doBootstrap config option

--- config/quickLookIsr.py
+++ config/quickLookIsr.py
@@ -1,7 +1,7 @@
 config.doSaturation = True  # saturation very important for roundness measurement in qfm
 config.brighterFatterMaxIter = 2  # Uncomment this to remove test warning
 config.doDeferredCharge = False  # no calib for this yet
-config.doBootstrap = True
+config.doBootstrap = False
 config.doApplyGains = False
 config.doSuspect = False
 config.defaultSaturationSource = "CAMERAMODEL"
