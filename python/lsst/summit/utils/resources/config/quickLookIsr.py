20a447ba00695ad4471944817731dda6157ed0d5
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2026-01-26 12:20:25 -0800

    Remove non-existent doWrite option

--- python/lsst/summit/utils/resources/config/quickLookIsr.py
+++ python/lsst/summit/utils/resources/config/quickLookIsr.py
@@ -1,6 +1,5 @@
 # mypy: disable-error-code="name-defined"
 
-config.doWrite = False  # this task writes separately, no need for this
 config.doSaturation = True  # saturation very important for roundness measurement in qfm
 config.brighterFatterMaxIter = 2  # Uncomment this to remove test warning
 config.doDeferredCharge = False  # no calib for this yet
